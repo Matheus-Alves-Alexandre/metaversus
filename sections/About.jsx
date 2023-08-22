f5e20ab5145fc01954dc1a4c841a88a0499da413
Matheus Alexandre <m.alexandre016@gmail.com>
2023-08-22 20:47:59 -0300

    first commit

---
+++ sections/About.jsx
@@ -0,0 +1,26 @@
+/* eslint-disable quotes */
+
+"use client";
+
+import { motion } from "framer-motion";
+import { TypingText } from "../components/CustomTexts";
+
+import styles from "../styles";
+import { fadeIn, staggerContainer } from "../utils/motion";
+
+const About = () => (
+  <section className={`${styles.paddings} relative z-10`}>
+    <div className="gradient-02 z-0" />
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
+    >
+      <TypingText title="| About Us" textStyles="text-center" />
+    </motion.div>
+  </section>
+);
+
+export default About;
